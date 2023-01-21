53e6762e191ccc64bde953e4d27f193915bf0aa0
mine6753 <nomad6753@gmail.com>
2023-01-21 20:09:13 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,22 +1,6 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function(){
-    searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function(){
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function(){
-    searchEl.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder', '');
-});
-
 
 const badgeEl = document.querySelector('header .badges');
+const toTopEl= document.querySelector('#to-top');
 
 window.addEventListener('scroll', _.throttle(function(){
     console.log(window.scrollY);
@@ -26,18 +10,34 @@ window.addEventListener('scroll', _.throttle(function(){
         gsap.to(badgeEl, .6, {
             opacity: 0,
             display: 'none'
-        })
+        });
+        //버튼 보이기!
+        gsap.to(toTopEl, .2, {
+            x: 0
+        });
     } else {
         //배지 보이기
         gsap.to(badgeEl, .6, {
             opacity: 1,
             display: 'block'
-        })
+        });
+        //버튼 숨기기!
+        gsap.to(toTopEl, .2, {
+            x: 100
+        });
+        //
     }
 }, 300));
-
 // https://cdnjs.com/libraries/lodash.js 출처
 // _.throttle(함수, 시간)
+
+
+toTopEl.addEventListener('click', function(){
+    gsap.to(window, .7, {
+        scrollTo:0
+    });
+});
+
 
 const fadeEls = document.querySelectorAll('.visual .fade-in');
 fadeEls.forEach(function (fadeEl, index) {
@@ -130,3 +130,4 @@ spyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl,'show')
     .addTo(new ScrollMagic.Controller());
 });
+
